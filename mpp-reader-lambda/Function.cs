5106101f8c4b8be4e881395fe075bf12a8d7a2bd
Onur Cetinkol <onurcetinkol@gmail.com>
2023-07-30 11:18:46 +0300

    update

--- mpp-reader-lambda/Function.cs
+++ mpp-reader-lambda/Function.cs
@@ -1,4 +1,10 @@
 using Amazon.Lambda.Core;
+using Amazon.S3.Model;
+using Amazon.S3;
+
+using net.sf.mpxj.reader;
+using Task = net.sf.mpxj.Task;
+using Amazon.Runtime.Internal.Util;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -6,16 +12,42 @@ using Amazon.Lambda.Core;
 namespace mpp_reader_lambda;
 
 public class Function
-{
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        AmazonS3Client client = new AmazonS3Client();
+
+        string objectName = "home_backup/flex.mpp";
+        string filePath = @"C:\sil";
+
+        bool result = AsyncHelpers.RunSync<bool>(() => GetFile.DownloadObjectFromBucketAsync(
+            client,
+            "realitifiles",
+            objectName,
+            filePath
+        ));
+
+        //string result = System.IO.Path.GetTempPath();
+
+        string testName = string.Empty;
+
+        if (result)
+        {
+            var project = new UniversalProjectReader().read($"{filePath}\\{objectName}");
+
+            foreach (Task task in project.Tasks)
+            {
+                testName += task.Name.ToString();
+                break;
+            }
+        }
+        else
+        {
+            testName = "result is false";
+        }
+
+
+        Console.WriteLine(result);
+
+        return testName;
     }
 }
---
+++ mpp-reader-lambda/GetFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3.Model;
+using Amazon.S3;
+
+namespace mpp_reader_lambda
+{
+    internal class GetFile
+    {
+        public static async Task<bool> DownloadObjectFromBucketAsync(
+            IAmazonS3 client,
+            string bucketName,
+            string objectName,
+            string filePath)
+        {
+            // Create a GetObject request
+            var request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectName,
+            };
+
+
+            //filePath = System.IO.Path.GetTempPath();
+
+            // Issue request and remember to dispose of the response
+            using GetObjectResponse response = await client.GetObjectAsync(request);
+
+            try
+            {
+                //return true;
+
+                // Save object to local file
+                await response.WriteResponseStreamToFileAsync($"{filePath}\\{objectName}", true, CancellationToken.None);
+                return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error saving {objectName}: {ex.Message}");
+                return false;
+            }
+
+            //// Issue request and remember to dispose of the response
+            //using (GetObjectResponse response = await client.GetObjectAsync(request))
+            //{
+            //    using (StreamReader reader = new StreamReader(response.ResponseStream))
+            //    {
+            //        string contents = reader.ReadToEnd();
+            //        Console.WriteLine("Object - " + response.Key);
+            //        Console.WriteLine(" Version Id - " + response.VersionId);
+            //        Console.WriteLine(" Contents - " + contents);
+
+            //        return true;
+            //    }
+            //}
+        }
+    }
+}
